f505bcff4dff7e6624e1b8144b657db423b7f1bd
Allan-Alexander <Allanalexander013@gmail.com>
2021-02-11 15:07:39 +0530

    Variables and Data Types

---
+++ variables.rb
@@ -0,0 +1,17 @@
+=begin
+
+  Allan Alexander
+  2:57pm
+  Thursday 11th Feb 2021
+
+  Topic : Variables And Data Types
+
+=end
+
+count   = 100           # Holds Integer
+course  = 'SaaS 2021'   # Holds String
+is_live = false          # boolean
+lessons = ['Basics', 'Intermediate', 'Advanced']
+
+puts " #{count} \n #{course} \n #{is_live} \n #{lessons}"
+puts "\n #{count.class} \n #{course.class} \n #{is_live.class} \n #{lessons.class}"
