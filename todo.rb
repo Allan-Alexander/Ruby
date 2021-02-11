7f3167f2a09eff8020fd0d5af3fbbb2142f9527d
Allan-Alexander <allanalexander013@gmail.com>
2021-02-11 14:08:36 +0530

    My first commit

---
+++ todo.rb
@@ -0,0 +1,12 @@
+
+# To print data.
+# var - next_todo (type by default)
+
+puts "What is your next todo?"
+next_todo = gets
+
+# puts "So, this is your next todo:"
+# puts next_todo
+
+puts "\n\n\n"
+puts "[ ] #{next_todo}" # String interpolation
