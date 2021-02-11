3330e6d3781f9d35bc6778f265a38173f9dd0a0b
Allan-Alexander <Allanalexander013@gmail.com>
2021-02-11 15:48:01 +0530

    Variables, Methods and Functions

--- variables.rb
+++ variables.rb
@@ -7,8 +7,16 @@
   Topic : Variables And Data Types
     Important: ".class" to get the type of data stored
                 no class for Boolean. Its either TrueClass pr FalseClass
+
                 "to_s" to convert from Int to String
                 "to_f" to convert Int to Float
+                ".upcase" to convert String to Uppercase
+                ".downcase" to convert String to Lowercase
+                ".capitalize" to convert the first char in a String
+                ".length" to calculate the string length
+                " if - elsif - else " if statements
+                " .casecmp " to compare String case insensitive
+
                 no need to use "to_s" while using String interpolation "#{age}"
 
 =end
@@ -34,3 +42,25 @@ puts "\n I am #{name} and my age is #{age.to_s} old"
 nums = [5,10,15]
 avg  = "#{ nums.sum / nums.length }".to_f
 puts " The average is #{avg}"
+
+# Boolean Values and If Statement
+
+puts 1 == 1
+puts "this" == "that"
+
+gener = "Fiction"
+puts "Fiction or Non-Fiction ?"
+
+if genre == "Friction"
+  puts "Try Legend Of Blue Sea"
+elsif genre == "Non-Friction"
+  puts "You should try John Wick"
+else
+  puts "Oh I dont know that genre"
+
+# Compare case insensitive String
+
+if "Allan".casecmp("ALLAN") == 0
+  puts "The User is Allan"
+else
+  puts "The user is not Allan"
