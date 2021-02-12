01abdb66c55b6cb27d60e7ff035796614ad723fc
Allan-Alexander <Allanalexander013@gmail.com>
2021-02-12 16:46:53 +0530

    Class

---
+++ class.rb
@@ -0,0 +1,79 @@
+=begin
+
+   Allan Alexander
+   3:21 pm
+   Friday, 12th February 2021
+
+   Topic: Class in Ruby
+     Important:
+        Class name starts with caps.
+=end
+
+
+# ******* Class *********
+
+class Student
+
+  #Constructor
+  def initialize(roll_no, name)
+    @roll_no     = roll_no
+    @name        = name
+    @is_enrolled = false
+  end
+
+  #Setter
+  def enroll
+    return  @is_enrolled = true
+  end
+
+  #Getter
+  def is_enrolled?
+    return @is_enrolled
+  end
+
+  #Getter
+  def name
+    return @name
+  end
+
+  #Getter
+  def roll_no
+    return @roll_no
+  end
+
+
+end
+
+student = Student.new(715,"Raj")
+p student.enroll
+
+# ******* Class *********
+
+# ******* Person Class *******
+class Person
+
+  #Contructor
+  def initialize(firstName, lastName)
+    @firstName = firstName
+    @lastName  = lastName
+  end
+
+  #Grettings Method
+  def grettings()
+    return "Welcome #{@firstName.capitalize} #{@lastName.capitalize}"
+  end
+
+  #Setter
+  def newPerson(firstName, lastName)
+    @firstName = firstName
+    @lastName  = lastName
+  end
+
+end
+
+Tom = Person.new("Tom","Sawyer")
+puts Tom.grettings()
+Tom.newPerson("Percy","Jackson")
+puts Tom.grettings()
+
+#********* Person ***********
